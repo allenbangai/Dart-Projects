bf38de616646fca38b63c726b759a6bf9db3f673
allenbangai <allenkamadje@gmail.com>
2022-02-19 22:19:17 +0100

    Dart basics: using boolean in dart part 1

---
+++ Dart Basics/boolean.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  bool val = true;
+  print(val);
+
+  val = false;
+  print(val);
+
+  var num1 = 4;
+  var num2 = 5;
+  val = num1 == num2;
+  print(val);
+
+  val = num1 != num2;
+  print(val);
+}
