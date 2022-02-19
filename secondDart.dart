47b1a98a2cb86d2c5d16f86e92b4f96459cbb09c
allenbangai <allenkamadje@gmail.com>
2022-02-19 15:58:04 +0100

    second dart code

---
+++ secondDart.dart
@@ -0,0 +1,11 @@
+class JavaCode
+{
+    static void main(List<String>args)
+    {
+        //  Your code
+        print("Enter your java code");
+    }
+}
+void main(List< String > args){
+	JavaCode.main(args);
+}
